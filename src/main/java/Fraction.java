c3f579d48217cd4a874f42fe9ce89c2be5acae19
lrama-git <leonitarama3@gmail.com>
2022-10-05 08:19:32 +0200

    Änderungen

--- src/main/java/Fraction.java
+++ src/main/java/Fraction.java
@@ -1,13 +1,13 @@
 public class Fraction {
 
-    private int dividend =1;
+    private int dividend = 1;
     private int divisor = 1;
 
-    //Constructur
-    public Fraction(int dividend, int divisor){
-        this.dividend= dividend;
-        this.divisor= divisor;
+    public Fraction(int dividend, int divisor) {
+        this.dividend = dividend;
+        this.divisor = divisor;
     }
+
 
     public int getDividend() {
         return dividend;
@@ -25,9 +25,96 @@ public class Fraction {
         this.divisor = divisor;
     }
 
+    public Fraction add(Fraction other) {
+        /**
+         * @ToDo
+         * Add this with other
+         */
+
+        //nummerator oben
+        //denumerator unten
+
+        //dividend oben
+        //divisor unten
+
+        return new Fraction(this.dividend * other.divisor + other.dividend * this.divisor, this.divisor * other.divisor);
+    }
+
+    public Fraction subtract(Fraction other) {
+        /**
+         * @ToDo
+         * subtract this with other
+         */
+
+        return new Fraction(this.dividend * other.divisor - other.dividend * this.divisor, this.divisor * other.divisor);
+    }
+
+    public Fraction multiply(Fraction other) {
+        /**
+         * @ToDo
+         * multiply this with other
+         */
+
+        return new Fraction((this.dividend * other.dividend), this.divisor * other.divisor);
+    }
+
+    public Fraction divide(Fraction other) {
+        /**
+         * @ToDo
+         * divide this with other
+         * divisor * jeweils anderen divident
+         */
+
+        Fraction result = new Fraction(1, 1);
+
+        result.setDivisor(this.getDivisor() * other.getDividend()); //Kreuzrechnen
+        result.setDividend(this.getDividend() * other.getDivisor()); //Kreuzrechnen
+
+        return result;
+        //return new Fraction((this.dividend*other.dividend), this.divisor/other.divisor);
+    }
+
+    public Fraction shorten() {
+        /**
+         * @ToDo
+         * shorten the Fraction
+         */
+
+        //-----------------------------------------------ggT
+
+        int ggT = 0;
+
+        int gg1 = this.getDividend();
+        int gg2 = this.getDivisor();
+
+        while (gg2 != 0) {
+            if (gg1 > gg2) {
+                gg1 = gg1 - gg2;
+            } else {
+                gg2 = gg2 - gg1;
+            }
+        }
+
+        ggT = gg1;  //der größte gemeinsame Teiler herausfinden!!
+
+
+        //-----------------------------------------------ggT
+
+        Fraction result = new Fraction((this.getDividend() / ggT), (this.getDivisor() / ggT));
+
+        return result;
+
+        //this.other -->
+        //ergebnis einen neuen bruch der ein
+        // this wäre 4/10
+        //other wäre 7/8
+        //28/80 --> new Fraction
+    }
+
     @Override
     public String toString() {
         return this.dividend + " / " + this.divisor;
     }
+
 
 }
--- src/test/java/FractionTest.java
+++ src/test/java/FractionTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,41 +7,100 @@ class FractionTest {
 
     @org.junit.jupiter.api.Test
     void getDividend() {
-        Fraction f = new Fraction(1,10);
+        Fraction f = new Fraction(1, 10);
 
-        Assertions.assertEquals(1,f.getDividend());
+        Assertions.assertEquals(1, f.getDividend());
     }
 
     @org.junit.jupiter.api.Test
     void setDividend() {
-        Fraction f = new Fraction(1,10);
+        Fraction f = new Fraction(1, 10);
         f.setDividend(2);
 
         //erwarte dass dividend 2 ist
-        Assertions.assertEquals(2,f.getDividend());
+        Assertions.assertEquals(2, f.getDividend());
     }
 
     @org.junit.jupiter.api.Test
     void getDivisor() {
-        Fraction f = new Fraction(1,10);
+        Fraction f = new Fraction(1, 10);
 
         //du erwartest dass divisor 10 ist
-        Assertions.assertEquals(10,f.getDivisor());
+        Assertions.assertEquals(10, f.getDivisor());
     }
 
     @org.junit.jupiter.api.Test
     void setDivisor() {
-        Fraction f = new Fraction(1,10);
+        Fraction f = new Fraction(1, 10);
         f.setDivisor(20);
 
 
-        Assertions.assertEquals(20,f.getDivisor());
+        Assertions.assertEquals(20, f.getDivisor());
     }
+
 
     @org.junit.jupiter.api.Test
     void testToString() {
-        Fraction f = new Fraction(17,3);
+        Fraction f = new Fraction(17, 3);
 
-        Assertions.assertEquals("17 / 3",f.toString());
+        Assertions.assertEquals("17 / 3", f.toString());
+    }
+
+
+    @Test
+    void add() {
+        Fraction f = new Fraction(3, 2);
+        Fraction other = new Fraction(4, 3);
+        /**
+         *  --> 9/6
+         *  --> 8/6
+         */
+
+        Fraction res = f.add(other);
+
+        Assertions.assertEquals("17 / 6", res.toString());
+
+    }
+
+    @Test
+    void subtract() {
+
+        Fraction f = new Fraction(6, 2);
+        Fraction other = new Fraction(4, 3);
+
+        Fraction res = f.subtract(other);
+
+        Assertions.assertEquals("10 / 6", res.toString());
+
+    }
+
+    @Test
+    void multiply() {
+        Fraction f = new Fraction(3, 2);
+        Fraction other = new Fraction(4, 3);
+
+        Fraction res = f.multiply(other);
+
+        Assertions.assertEquals("12 / 6", res.toString());
+    }
+
+    @Test
+    void divide() {
+        Fraction f = new Fraction(3, 2);
+        Fraction other = new Fraction(4, 3);
+
+        Fraction res = f.divide(other);
+
+        Assertions.assertEquals("9 / 8", res.toString());
+    }
+
+    @Test
+    void shorten() {
+
+        Fraction f = new Fraction(18, 27);
+
+        Fraction res = f.shorten();
+
+        Assertions.assertEquals("2 / 3", res.toString());
     }
 }
